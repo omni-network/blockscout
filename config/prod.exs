727026dcfe1bd853feb06d22cabe228c5536c380
corver <corver@omni.network>
2023-10-25 15:09:59 +0200

    significantly reduce file logs (#24)

--- config/prod.exs
+++ config/prod.exs
@@ -7,14 +7,14 @@ config :logger, :console, level: :info
 config :logger, :ecto,
   level: :info,
   path: Path.absname("logs/prod/ecto.log"),
-  rotate: %{max_bytes: 52_428_800, keep: 19}
+  rotate: %{max_bytes: 1_048_576, keep: 1}
 
 config :logger, :error,
   path: Path.absname("logs/prod/error.log"),
-  rotate: %{max_bytes: 52_428_800, keep: 19}
+  rotate: %{max_bytes: 1_048_576, keep: 1}
 
 config :logger, :account,
   level: :info,
   path: Path.absname("logs/prod/account.log"),
-  rotate: %{max_bytes: 52_428_800, keep: 19},
+  rotate: %{max_bytes: 1_048_576, keep: 1},
   metadata_filter: [fetcher: :account]
